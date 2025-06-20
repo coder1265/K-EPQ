ebae6328a5d6d9e0ca295f34b5711f0c7876d78b
Coder1265 <kester@3fpellets.com>
2025-06-20 18:12:44 +0100

    Commit with right sidebar working on 20/6/2025

--- js/main.js
+++ js/main.js
@@ -1,57 +1,57 @@
 "use strict";
-window.addEventListener("DOMContentLoaded",()=>{
-	document.getElementById("resource-list").style.display = "none";
-//	code for the navigation bar resources
-	document.getElementById("navResources").addEventListener("mousedown",function showMoreNav(){
-		if(document.getElementById("resource-list").style.display == "none"){
-			document.getElementById("resource-list").style.display = "block";
-			document.getElementById("resouceArrow").innerHTML = "▲";
-		} else{
-			document.getElementById("resource-list").style.display = "none";
-			document.getElementById("resouceArrow").innerHTML = "▼";
-		}
-	});
+window.addEventListener("DOMContentLoaded", () => {
+  let isThrottled = false;
+  const throttleDelay = 100; // scroll interval
 
+  window.addEventListener("scroll", (event) => {
+    if (!isThrottled) {
+      isThrottled = true;
 
-let isThrottled = false;
-const throttleDelay = 100; // scroll interval 
+      // Execute your scroll handling logic here
+      contactScrolledBy();
 
-window.addEventListener("scroll", (event) => {
-  if (!isThrottled) {
-    isThrottled = true;
-
-    // Execute your scroll handling logic here
-    contactScrolledBy();
-
-    // After the delay, allow the function to be executed again
-    setTimeout(() => {
-      isThrottled = false;
-    }, throttleDelay);
-  }
-  // If isThrottled is true, the scroll event is ignored until the timeout finishes
-});
-const quickContactElement = document.getElementById("quick-contact");
-function contactScrolledBy() {
-  if (window.scrollY > 50) {
-    if (quickContactElement) {
-      quickContactElement.style.display = "none";
+      // After the delay, allow the function to be executed again
+      setTimeout(() => {
+        isThrottled = false;
+      }, throttleDelay);
     }
-  } else {
-    if (quickContactElement) {
+    // If isThrottled is true, the scroll event is ignored until the timeout finishes
+  });
+  const quickContactElement = document.getElementById("quick-contact");
+  function contactScrolledBy() {
+    if (window.scrollY > 50) {
+      if (quickContactElement) {
+        quickContactElement.classList.add("animate1");
+      }
+    } else {
+      if (quickContactElement) {
+        quickContactElement.classList.remove("animate1");
+        quickContactElement.style.setProperty("opacity", "1");
+        quickContactElement.style.display = "inline-flex";
+      }
+    }
+  }
+  setInterval(checkTop, 100);
+
+  function checkTop() {
+    if (window.scrollY > 50) {
+      quickContactElement.classList.add("animate1");
+    } else {
+      quickContactElement.classList.remove("animate1");
+      quickContactElement.style.setProperty("opacity", "1");
       quickContactElement.style.display = "inline-flex";
     }
   }
-}
-	setInterval(checkTop,100);
-	function checkTop() {
-	if (window.scrollY > 50) {
-		    quickContactElement.style.display = "none";
-	  } else {
-			quickContactElement.style.display = "inline-flex";
-	  }
-	}
-  function sidenav(){
-    
-  }
 
+  function sidenav() {
+    console.log("sidenav has been clicked");
+  }
+  document.getElementById("navMenu").addEventListener("click", () => {
+    document.getElementById("sidebar").classList.remove("animation2-close");
+    document.getElementById("sidebar").classList.add("animation2");
+  });
+  document.getElementById("closeSidebar").addEventListener("click", () => {
+    document.getElementById("sidebar").classList.remove("animation2");
+    document.getElementById("sidebar").classList.add("animation2-close");
+  });
 });
