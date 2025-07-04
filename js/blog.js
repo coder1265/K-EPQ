1e329314d00df94b8cf5dbdf6dc1e28fd7f6151c
Coder1265 <kester@3fpellets.com>
2025-07-04 18:00:06 +0100

    This had got the blog page working.

--- js/blog.js
+++ js/blog.js
@@ -1,123 +1,46 @@
 "use strict";
 console.log("Blog script loaded");
-// let id = 0;
 const blogContainer = document.getElementById("blog-container");
-const bloglist = document.getElementById("blog-list");
-class Blog {
-  constructor() {
-    this.blogs = [];
-  }
+const blogList = document.getElementById("blog-list");
 
-  addBlog(blog) {
-    this.blogs.push(blog);
-  }
-
-  getBlogs() {
-    return this.blogs;
-  }
-
-  findBlogById(id) {
-    return this.blogs.find((blog) => blog.id === id);
-  }
-}
 class BlogPost {
-  static nextId = 1;
-  constructor(title, content, author, date) {
-    this.id = BlogPost.nextId++;
+  constructor(title, contentUrl, author, date) {
     this.title = title;
-    this.content = content;
+    this.contentUrl = contentUrl;
     this.author = author;
     this.date = date;
   }
-
-  getSummary() {
-    return this.title;
-  }
 }
-const text1 = `We are excited to announce the launch of our new website prototype!
-This site now has payment capabilities and most pages are functional, however features still outstanding include user registration and login, and the ability to add products to a cart, a propper domain name, server-side functionality, and a products page. 
-We are working hard to get these features implemented as soon as possible, so stay tuned for updates.<br>
-This site was built using vanilla JavaScript, HTML, and CSS, and is hosted on GitHub Pages.
-The development software used has changed quite a bit however the main tools used were:
-Code editors:
-<ul>
-<li>Visual Studio Code
-<ul>
-<li>Live Server extension</li>
-<li>Prettier extension</li>
-<li>intelliSense</li>
-<li>Emmet</li>
-<li>Microsoft Edge DevTools</li>
-<li>git hub code spaces</li>
-<li>GitHub Copilot</li>
-</ul>
-</li>
-<li>Adobe dreamweaver CC</li>
-<li>Notepad</li>
-</ul>
-Graphics software:
-<ul>
-<li>Adobe Photoshop CC</li>
-<li>Paint</li>
-</ul>
-Version control:
-<ul>
-<li>GitHub</li>
-<li>file explorer (Zip files)</li>
-</ul>
-Trialed software:
-XAMPP:
-<p>It was a free and easy to use local server software, and accessable for everyone in the wifi network, although i didn't know how to make it globaly accessable</p>
-Packet riot:
-<p>I was a free method of hosting your own website, however it was rather complex and insecure (which i didn't know how to fix)</p>
-Localhost:
-<p>This was using VS Code to run a local server, it was very similar to XAMPP but more complex and had the same problems (it also didn't have a database)</p>
-W3Schools spaces:
-<p>This was a free hosting service that allowed you to host your own website, however it was very limited and didn't allow for server-side functionality</p>
-GitHub Pages:
-<p>This is a free hosting service that allows you to host your own website, however it is limited to static sites and doesn't allow for server-side functionality but it is better than W3 spaces as it allows for version control(using git)</p>
-<br>
-This site was buit only using HTML, CSS, and JavaScript, and is hosted on GitHub Pages No frameworks were used however stripe intergrations were used fot the payment options.
-`;
-const text2 = `We have recently purchased 30 new chickens for our farm!<br>
-15 of these are burford brown chickens, which are known for their high quality egg production and friendly temperament. <br>
-The other 15 are Dekalb white chickens, which are known for their high egg production and hardiness although they are very active. <br>
-<br>
-<img src="/Images/chicken-profiles/Chicken4.jpg" alt="A picture of a chicken" width="300"><br>
-<p> Above is a picture of one of our new burford brown chickens, they are very friendly and love to be around humans (and have lots of treats)</p>
-<img src="Images/chicken-profiles/Chicken13.jpg" alt="A picture of a chicken" width="300"><br>
-<p>Above is a picture of one of our new Dekalb white chickens, they are very active and love to run around (and have lots of treats)</p>
-`;
-const blog = new Blog();
-const blogPost1 = new BlogPost(
-  "New Prototype Launch",
-  text1,
-  "Kester",
-  "2025-06-23"
+
+let posts = [];
+posts.push(
+  new BlogPost(
+    "New Prototype website",
+    "/Blog%20posts/post1.txt",
+    "Kester Franklin",
+    "2025-7-04"
+  )
 );
-const blogPost2 = new BlogPost(
-  "New Chickens bought",
-  text2,
-  "Kester",
-  "2025-06-23"
+posts.push(
+  new BlogPost(
+    "Our new hens",
+    "/Blog%20posts/post2.txt",
+    "Kester Franklin",
+    "2025-05-09"
+  )
 );
 
-blog.addBlog(blogPost1);
-blog.addBlog(blogPost2);
-console.log(blog);
 function addToDocument() {
-  blog.getBlogs().forEach((post, i) => {
-    console.log(post);
-    bloglist.innerHTML += `<p><a id="bl${post.id}" title="${post.title}">${post.title}</a></p>`;
-    document
-      .getElementById(`bl${post.id}`)
-      .addEventListener("click", function () {
-        blogContainer.innerHTML = `<h2>${post.title}</h2>
-      <p><strong>Author:</strong> ${post.author}</p>
-      <p><strong>Date:</strong> ${post.date}</p>
-      <p>${post.content}</p>`;
-      });
-  });
+  for (const post of posts) {
+    const postElement = document.createElement("div");
+    postElement.classList.add("blog-title");
+    postElement.innerHTML = `<p>${post.title}</p>`;
+    blogList.appendChild(postElement);
+    postElement.addEventListener("click", () => {
+      getPostData(post.contentUrl);
+    });
+    console.log(`Added post: ${post.title}`);
+  }
 }
 
 addToDocument();
@@ -131,10 +54,9 @@ async function getPostData(input) {
       throw new Error(`Response status: ${response.status}`);
     }
     const text = await response.text();
-    console.log(text);
+    blogContainer.innerHTML = text;
+    console.log("Data fetched successfully");
   } catch (error) {
     console.error(error.message);
   }
 }
-
-getPostData("/Blog%20posts/post1");
